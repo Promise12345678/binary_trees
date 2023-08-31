5e09cbf9e477c270b7b374f755c7d048a1cb6c8c
Promise12345678 <promisesmutenhaunga@gmail.com>
2023-08-31 16:56:45 -0700

    Attack

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,26 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- *
- * @parent: pointer to the parent node
- *
- * @value: value assigned to the new node
- *
- * Return: A new node or NULL if fails.
+ * binary_tree_node - creates a node
+ * @parent: binary_tree_t node
+ * @value: int variable
+ * Return: new node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *created_node = NULL;
+	binary_tree_t *new;
 
-	created_node = malloc(sizeof(binary_tree_t));
-	if (!created_node)
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
 		return (NULL);
-	created_node->n = value;
-	created_node->parent = parent;
-	created_node->left = NULL;
-	created_node->right = NULL;
 
-	return (created_node);
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	new->n = value;
+	return (new);
 }
